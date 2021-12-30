68e2d162ca0553dfe7b2057273ee0c09f50719a0
Enes Arda <enesarda@Eness-MacBook-Air-4.local>
2021-12-30 20:39:26 +0300

    add comment

--- get_features.m
+++ get_features.m
@@ -1,6 +1,6 @@
 function [features] = get_features(segment, Fs)
 
-    features = zeros(1, 46);
+    features = zeros(1, 46); % feature vector is initialized
 
     features(1, 1:4) = hpss_features(segment, Fs); % hpss features are added
     features(1, 5:6) = rms_features(segment, Fs); % rms features are added
